4e8490bfb81702a292331bdb06676c64366beb15
Ruqayya1 <ruqayya.9428@gmail.com>
2018-11-07 21:49:58 -0600

    home

--- dom_manipulation/homework.js
+++ dom_manipulation/homework.js
@@ -1,30 +1,46 @@
 
-
-// adding lists to the item
-
-//  const val =document.getElementById("task").value;
-//  val.className='collection-item';
-//  document.querySelector('ul.collectiion').appendChild(val);
-//  const link = document.createElement('a');
-//  link.classNmae ='delete-item secondary-content';
-//  link.setAttribute('href','#');
-//  link.innerHTML='<i class = "fa fa-remove"></i>'
-//  val.appendChild(link);
-   
-// const taskinput = document.getElementById('task');
-// taskinput.value=" ";
-document.querySelector('.btn').addEventListener('click', onclick);
- function onclick(){
-   var li = document.createElement("li");
-   var input = document.getElementById("task");
-   li.innerHTML =input.value;
-   li.className='delete-item secondary-content';
-   const link = document.createElement('a');
-   link.className ='delete-item secondary-content';
-   li.setAttribute('href','#');
-   link.innerHTML='<i class = "fa fa-remove"></i>'
-   li.appendChild(link);
-   document.querySelector('ul.collection').appendChild(li);
+// document.querySelector('.btn').addEventListener('click', onclick);
+//  function onclick(){
+//    var li = document.createElement("li");
+//    var input = document.getElementById("task");
+//    li.innerHTML =input.value;
+//    li.className='delete-item secondary-content';
+//    const link = document.createElement('a');
+//    link.className ='delete-item secondary-content';
+//    li.setAttribute('href','#');
+//    link.innerHTML='<i class = "fa fa-remove"></i>'
+//    li.appendChild(link);
+//    document.querySelector('ul.collection').appendChild(li);
    
 
+// }
+
+
+// document.body.addEventListener('click',strikes)
+// function strikes(e){
+//     if(e.target.parentElement.className==="delete-item secondary-content"){
+//         let val = document.getElementsByClassName("collection-item").value
+//         val.style.background ="red";
+//     }
+// }
+
+// >>>>>>>>>>>>>>New way>>>>>>>>>///
+
+const val= document.querySelector(".btn")
+val.addEventListener('click', event);
+function event(){
+    var txtval = document.getElementById('task').Value;
+    var listNode = document.getElementsByClassName('collection');
+    var liNode =document.createElement("li");
+    var txtNode =document.createTextNode(txtval);
+    liNode.className = 'collection-item';
+    const link = document.createElement('a');
+  link.className ='delete-item secondary-content';
+   liNode.setAttribute('href','#');
+       link.innerHTML='<i class = "fa fa-remove"></i>'
+   
+    liNode.appendChild(txtNode);
+    liNode.appendChild(link);
+         listNode.appendChild(liNode);
+         preventDefault();
 }
